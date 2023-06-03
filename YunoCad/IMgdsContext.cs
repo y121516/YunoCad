5ed3c1b9c008d5e863291acd56e60808b8bb5ea1
Yuhki Ogasawara <y121516@gmail.com>
2023-06-03 15:48:00 +0900

    IMgdsContext.ScreenUpdateMode を実装する

---
+++ YunoCad/IMgdsContext.cs
@@ -0,0 +1,13 @@
+﻿using Informatix.MGDS;
+
+namespace YunaComputer.YunoCad;
+
+public interface IMgdsContext : IGlobalContext
+{
+    private class MgdsContext : IMgdsContext { }
+    private static readonly IMgdsContext instance = new MgdsContext();
+    static new IMgdsContext Instance => instance;
+
+    void ScreenUpdateMode(ScreenUpdate updateMode)
+        => Cad.ScreenUpdateMode(updateMode);
+}
---
+++ YunoCadTest/IMgdsContextTest.cs
@@ -0,0 +1,20 @@
+﻿using Informatix.MGDS;
+using YunaComputer.YunoCad;
+
+namespace YunaComputer.YunoCadTest;
+
+[TestClass]
+public class IMgdsContextTest
+{
+    [TestMethod]
+    public void ScreenUpdateModeTest()
+    {
+        var mc = IMgdsContext.Instance;
+        var id = mc.StartMicroGDS();
+        using var c = new Conversation();
+        c.Start(id, 5 * 1000);
+        mc.ScreenUpdateMode(ScreenUpdate.Interactive);
+        mc.ScreenUpdateMode(ScreenUpdate.Bulk);
+        Cad.Exit(Save.DoNotSave, Save.DoNotSave);
+    }
+}
