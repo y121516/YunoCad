1786e8a457a76504f9761fe073cae7d3b1f20629
Yuhki Ogasawara <y121516@gmail.com>
2023-06-03 15:56:46 +0900

    IDocumentContext.SetCursorFromFile を実装する

---
+++ YunoCad/IDocumentContext.cs
@@ -0,0 +1,12 @@
+﻿using Informatix.MGDS;
+
+namespace YunaComputer.YunoCad;
+
+public interface IDocumentContext : IMgdsContext
+{
+    private class DocumentContext : IDocumentContext { }
+    private static readonly IDocumentContext instance = new DocumentContext();
+    static new IDocumentContext Instance => instance;
+
+    void SetCursorFromFile(string fileName) => Cad.SetCursorFromFile(fileName);
+}
---
+++ YunoCadTest/IDocumentContextTest.cs
@@ -0,0 +1,21 @@
+﻿using Informatix.MGDS;
+using YunaComputer.YunoCad;
+
+namespace YunaComputer.YunoCadTest;
+
+[TestClass]
+public class IDocumentContextTest
+{
+    [TestMethod]
+    public void SetCursorFromFileTest()
+    {
+        var ctx = IDocumentContext.Instance;
+        var id = ctx.StartMicroGDS();
+        using var c = new Conversation();
+        c.Start(id, 5 * 1000);
+        Cad.CreateMANFile();
+        Cad.CloseView();
+        ctx.SetCursorFromFile("");
+        Cad.Exit(Save.DoNotSave, Save.DoNotSave);
+    }
+}
