28f9e956779925cccb1b2ee929dea06159672607
Yuhki Ogasawara <y121516@gmail.com>
2023-06-06 18:22:42 +0900

    IDrawingWindowContext.CreateLayer を実装する

---
+++ YunoCad/IDrawingWindowContext.cs
@@ -0,0 +1,13 @@
+﻿using Informatix.MGDS;
+
+namespace YunaComputer.YunoCad;
+
+public interface IDrawingWindowContext : IDocumentContext
+{
+    private class DrawingWindowContext : IDrawingWindowContext { }
+    private static readonly IDrawingWindowContext instance = new DrawingWindowContext();
+    static new IDrawingWindowContext Instance => instance;
+
+    void CreateLayer(string layerName, string aliasName = "") =>
+        Cad.CreateLayer(layerName, aliasName);
+}
---
+++ YunoCadTest/IDrawingWindowContextTest.cs
@@ -0,0 +1,22 @@
+﻿using Informatix.MGDS;
+using YunaComputer.YunoCad;
+
+namespace YunaComputer.YunoCadTest;
+
+[TestClass]
+public class IDrawingWindowContextTest
+{
+    [TestMethod]
+    public void CreateLayerTest()
+    {
+        var ctx = IDocumentContext.Instance;
+        var id = ctx.StartMicroGDS();
+        using var c = new Conversation();
+        c.Start(mgds =>
+        {
+            mgds.CreateManFile();
+            Cad.CreateLayer("newLayer", "");
+            mgds.Exit();
+        }, id);
+    }
+}
