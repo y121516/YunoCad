721be6ddd6abc1dff810f6ef39f0284b73272d2d
Yuhki Ogasawara <y121516@gmail.com>
2023-06-06 15:39:20 +0900

    class ConversationExtensions を追加する

---
+++ YunoCad/ConversationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Informatix.MGDS;
+
+namespace YunaComputer.YunoCad;
+
+public static class ConversationExtensions
+{
+    private const int DefaultTimeout = 5 * 1000;
+
+    public static void Start(
+        this Conversation c,
+        Action<IMgdsContext> action,
+        int sessionID = Conversation.AnySession,
+        int timeoutMs = DefaultTimeout)
+    {
+        c.Start(sessionID, timeoutMs);
+        action(IMgdsContext.Instance);
+    }
+}
